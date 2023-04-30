d0f3e388c6b361fc7f735edcdc9f5cf273211bac
Marcel Wiessler <marcel@gaisterhand.de>
2023-04-30 16:13:58 +0200

    add unity

---
+++ src/needle/generated/gen.js
@@ -0,0 +1,17 @@
+﻿import "./register_types"
+
+export const needle_exported_files = new Array();
+globalThis["needle:codegen_files"] = needle_exported_files;
+needle_exported_files.push("static/empty.glb?v=1682864000964");
+
+document.addEventListener("DOMContentLoaded", () =>
+{
+	const needleEngine = document.querySelector("needle-engine");
+	if(needleEngine && needleEngine.getAttribute("src") === null)
+	{
+		needleEngine.setAttribute("hash", "1682864000964");
+		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
+	}
+});
+
+console.log("Made\ with\ ♥\ by\ 🌵\ Needle\ -\ https://needle\.tools\ —\ Version\ 3\.2\.15-exp");
